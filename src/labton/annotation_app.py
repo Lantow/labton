1eeefca6b6ad122e9cabe7698b668486f64280b8
Morten Lantow <mrlantow@gmail.com>
2022-11-02 18:33:40 +0100

    first attempt at making labton work from colab

--- src/labton/annotation_app.py
+++ src/labton/annotation_app.py
@@ -1,13 +1,17 @@
 from labton.labton_ui import flask_annotater_page
 from labton.labton_backend.config_file_handler import ConfigHandler
 from labton.labton_backend.data_handler import DatabaseHandler
+
 from flask import Flask
+from flask_ngrok import run_with_ngrok
 from pandas import DataFrame
+import sys
 import os
 
 class App(ConfigHandler):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.is_run_in_colab = 'google.colab' in sys.modules
     
     def run(self):
         data_source = self.config["data_source"]
@@ -32,6 +36,10 @@ class App(ConfigHandler):
         
         app = flask_annotater_page.return_app(
             project_name=self.config["project_name"])
+        
+        if self.is_run_in_colab:
+           run_with_ngrok(app) 
+        
         app.run(debug=False, 
                 host=app.config["host"], 
                 port=app.config["port"])
